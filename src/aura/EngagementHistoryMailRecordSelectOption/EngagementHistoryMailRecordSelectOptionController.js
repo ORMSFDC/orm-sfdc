e6a33a0252d9c509087b0334faafa8264a0c9b3a
rynop <ryan.pendergast@gmail.com>
2018-08-01 13:49:13 -0500

    remove manage packge PI

--- src/aura/EngagementHistoryMailRecordSelectOption/EngagementHistoryMailRecordSelectOptionController.js
+++
@@ -1,7 +0,0 @@
-({
-	handleRecordClick : function(component, event, helper) {
-		var evt = component.getEvent("EngagementHistoryPersonSelected");
-		evt.setParams({index: component.get("v.index")});
-		evt.fire();
-	}
-})
--- src/aura/OutlookCompose/OutlookComposeRenderer.js
+++
@@ -1,9 +0,0 @@
-({
-    afterRender: function(component, helper){
-        var eo = component.get("v.eo");
-        helper.registerRemoteActionEventHander(component);
-        if(eo.init) {
-            eo.init(component.getElement());
-        }
-    }
-})
