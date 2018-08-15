5dc23cbf0133cacfa545ef6cfbceb24594d8f34e
rynop <ryan.pendergast@gmail.com>
2018-08-15 14:27:44 -0500

    enable start application button in run cap qualifier

--- src/aura/ClientInfoCloneCMP/ClientInfoCloneCMPController.js
+++ src/aura/ClientInfoCloneCMP/ClientInfoCloneCMPController.js
@@ -563,6 +563,10 @@
         }
     },
     start_newloan:function(component){
+        //Dont save info, just send them to SAL splash screen
+        window.open('/s/startnewloan');
+        return;
+
         //    alert('ClientInfo start_newloan ');
         component.set('v.showSpinnerLoan',true);
         var getdate = component.get("v.ApplicationDate");
