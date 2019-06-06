399adefa7c80e430fd40646629cb862d78fd2590
DeekondaSahitya <40277588+DeekondaSahitya@users.noreply.github.com>
2019-06-06 15:54:08 -0700

    Update PartnerContactUsController.js

--- src/aura/PartnerContactUs/PartnerContactUsController.js
+++ src/aura/PartnerContactUs/PartnerContactUsController.js
@@ -1,5 +1,14 @@
 ({
 	myAction : function(component, event, helper) {
 		
+	},
+
+	navigateToURL : function(component, event, helper){
+		var URL = component.get("v.Link");
+		var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+            "url": URL
+        });
+		urlEvent.fire();
 	}
 })
